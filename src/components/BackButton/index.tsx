9187ac7834635a3c9c9ea9dd9872812d631e3e84
Hélvio Filho <helviosvf@gmail.com>
2022-05-02 23:26:55 -0300

    feat: created back button

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { BorderlessButtonProps } from 'react-native-gesture-handler'
+import { Container } from './styles';
+import { useTheme } from 'styled-components';
+
+interface BackButtonProps extends BorderlessButtonProps {
+  color?: string;
+}
+
+export function BackButton({ color, ...rest }: BackButtonProps) {
+  const theme = useTheme();
+
+  return (
+    <Container {...rest}>
+      <MaterialIcons
+        name="chevron-left"
+        size={24}
+        color={color ? color : theme.colors.text}
+      />
+    </Container>
+  );
+}
