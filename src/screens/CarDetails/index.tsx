cd5ed2587b36ca1618a576f0742fbd3c65f7895a
Hélvio Filho <helviosvf@gmail.com>
2022-05-04 20:19:46 -0300

    feat: created car details screen

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Accessory } from '../../components/Accessory';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Button } from '../../components/Button';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from './styles';
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => { }} />
+      </Header>
+      <CarImages>
+        <ImageSlider imagesUrl={['https://cdn.wheel-size.com/automobile/body/audi-rs5-2020-2022-1613028936.4473815.png']} />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 PH" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Este é um automóvel desportivo. Surgiu do lendário
+          touro de lide indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
+      <Footer>
+        <Button title="Confirmar" onPress={() => { }} />
+      </Footer>
+    </Container>
+  );
+}
