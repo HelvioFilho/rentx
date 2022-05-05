e83661634bb140f3444423646b1e3c9f27305d0d
Hélvio Filho <helviosvf@gmail.com>
2022-05-04 21:04:06 -0300

    Feat: Home done

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,15 +3,27 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
   Header,
   TotalCars,
   HeaderContent,
+  CarList
 } from './styles';
 
 export function Home() {
+  const car = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://cdn.wheel-size.com/automobile/body/audi-rs5-2020-2022-1613028936.4473815.png',
+  }
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +42,13 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => (
+          <Car data={car} />
+        )}
+      />
     </Container>
   );
 }
