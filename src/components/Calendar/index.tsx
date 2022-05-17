ed793651f09e70dcde6f2ee93a40040290b2f1b8
Hélvio Filho <helviosvf@gmail.com>
2022-05-17 17:39:39 -0300

    Refactor: New file created to separate calendar settings

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars';
 import { useTheme } from 'styled-components';
+import { ptBR } from './localeConfig';
 
-LocaleConfig.locales['pt-br'] = {
-  monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
-  today: 'Hoje'
-};
+LocaleConfig.locales['pt-br'] = ptBR;
 LocaleConfig.defaultLocale = 'pt-br';
 
 export function Calendar() {
@@ -45,6 +40,8 @@ export function Calendar() {
 
       firstDay={1}
       minDate={String(new Date())}
+      makingType="period"
+    // markedDates={}
     />
   );
 }
