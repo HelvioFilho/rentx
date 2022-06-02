774e6a989297c167c375d805fcd10d1700d57e07
Hélvio Filho <helviosvf@gmail.com>
2022-06-02 04:29:54 -0300

    Refactor(Home): don't show number of cars until API response

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -118,9 +118,12 @@ export function Home() {
             width={RFValue(108)}
             height={RFValue(12)}
           />
-          <TotalCars>
-            Total de {cars.length} carros
-          </TotalCars>
+          {
+            !loading &&
+            <TotalCars>
+              Total de {cars.length} carros
+            </TotalCars>
+          }
         </HeaderContent>
       </Header>
       {loading ?
