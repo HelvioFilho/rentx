0f049f3a01522b207c7da51dcc9720021c210961
Hélvio Filho <helviosvf@gmail.com>
2022-06-20 16:47:59 -0300

    Feat(app.tab.routes): navigation icons and colors done

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -4,26 +4,59 @@ import { MyCars } from '../screens/MyCars';
 import { Home } from '../screens/Home';
 import { AppStackRoutes } from './app.stack.routes';
 
+import HomeSvg from '../assets/tab/home.svg';
+import CarSvg from '../assets/tab/car.svg';
+import PeopleSvg from '../assets/tab/people.svg';
+
+import { useTheme } from 'styled-components';
+import { Platform } from 'react-native';
+
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
+
   return (
     <Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 78,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          backgroundColor: theme.colors.background_primary
+        },
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.text_detail
       }}
+
     >
       <Screen
         name="HomeBase"
         component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={24} height={24} fill={color} />
+          )
+        }}
       />
       <Screen
         name="Profile"
         component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
+          )
+        }}
       />
       <Screen
         name="MyCars"
         component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarSvg width={24} height={24} fill={color} />
+          )
+        }}
       />
     </Navigator>
   )
