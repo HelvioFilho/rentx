a9810450615806b646ac93f4934806d1e680ec11
Hélvio Filho <helviosvf@gmail.com>
2022-06-21 04:26:17 -0300

    Fix(Home): fixed memory leak

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -37,18 +37,26 @@ export function Home() {
   }
 
   useEffect(() => {
+    let isMounted = true;
     async function fetchCars() {
       try {
         const response = await api.get('/cars');
-        setCars(response.data);
+        if (isMounted) {
+          setCars(response.data);
+        }
       } catch (err) {
         console.log(err);
       } finally {
-        setLoading(false);
+        if (isMounted) {
+          setLoading(false);
+        }
       }
     }
 
     fetchCars();
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   return (
