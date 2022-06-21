6e086c915d6c3476fc613ce6d99779b90b7d3823
Hélvio Filho <helviosvf@gmail.com>
2022-06-20 21:10:47 -0300

    Chore: installing watermelonDB

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', 'inline-dotenv'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      'inline-dotenv',
+      ["@babel/plugin-proposal-decorators", { "legacy": true }]
+    ],
   };
 };
