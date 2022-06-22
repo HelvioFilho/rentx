6ed14e84af888b6a79153129ccf10adbb75a5097
Hélvio Filho <helviosvf@gmail.com>
2022-06-22 05:33:03 -0300

    Feat(Profile): updating offline data in watermelonDB

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
 import { BackButton } from '../../components/BackButton';
 import * as ImagePicker from 'expo-image-picker';
+import * as Yup from 'yup';
 
 import {
   Container,
@@ -22,18 +23,22 @@ import {
   Section,
 } from './styles';
 import { Input } from '../../components/Input';
-import { Keyboard, KeyboardAvoidingView } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, Modal } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { PasswordInput } from '../../components/PasswordInput';
 import { useAuth } from '../../hooks/auth';
+import { Button } from '../../components/Button';
+import { WarningModal } from '../../components/WarningModal';
 
 export function Profile() {
-  const { user, signOut } = useAuth();
+  const { user, signOut, updatedUser } = useAuth();
 
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
   const [avatar, setAvatar] = useState(user.photo ? user.photo : `https://ui-avatars.com/api/?bold=true&font-size=0.60&background=1B1B1F&color=fff&name=${user.name}&length=1`);
   const [name, setName] = useState(user.name);
   const [driverLicense, setDriverLicense] = useState(user.driver_license);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [visible, setVisible] = useState(false);
 
   const theme = useTheme();
   const { goBack } = useNavigation();
@@ -51,6 +56,41 @@ export function Profile() {
     }
     if (result.uri) {
       setAvatar(result.uri)
+    }
+  }
+
+  async function handleProfileUpdate() {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string()
+          .required('CNH não pode ser vazio'),
+        name: Yup.string()
+          .required('Nome não pode ser vazio')
+      });
+
+      const data = { name, driverLicense };
+      await schema.validate(data);
+
+      await updatedUser({
+        id: user.id,
+        email: user.email,
+        name,
+        driver_license: driverLicense,
+        photo: avatar,
+        token: user.token
+      });
+      setErrorMessage("Perfil atualizado!");
+      setVisible(true);
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        setErrorMessage(error.message);
+        setVisible(true);
+        if (!name) setName(user.name);
+        if (!driverLicense) setDriverLicense(user.driver_license);
+
+      } else {
+        console.log("não foi possível salvar as informações");
+      }
     }
   }
 
@@ -138,7 +178,32 @@ export function Profile() {
                   />
                 </Section>
             }
+            <Button
+              title="Salvar alterações"
+              onPress={handleProfileUpdate}
+            />
           </Content>
+          <Modal
+            animationType="fade"
+            transparent
+            visible={visible}
+            onRequestClose={() => setVisible(false)}
+          >
+            <WarningModal
+              title="Aviso"
+              height={170}
+              align
+              message={errorMessage}
+              button={[
+                {
+                  title: "Fechar",
+                  color: theme.colors.main,
+                  close: true,
+                },
+              ]}
+              closeModal={() => setVisible(false)}
+            />
+          </Modal>
         </Container>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView >
